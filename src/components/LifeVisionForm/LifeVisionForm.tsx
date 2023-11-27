3ae184dd31942f6537eb5c48ea6903ad47bda4f4
ddeath <dusan.plavak@gmail.com>
2023-11-27 23:46:04 +0800

    Add simple local notifications example

--- src/components/LifeVisionForm/LifeVisionForm.tsx
+++ src/components/LifeVisionForm/LifeVisionForm.tsx
@@ -1,15 +1,40 @@
 import { Button, Text, TextInput } from "react-native"
 import { useVisionStore } from "../../store"
 import { useState } from "react"
+import * as Notifications from 'expo-notifications'
+
+const sendLocalNotification = async () =>  {
+  const title = "Vision clarity";
+  const body = "Read your Vision in the morning to gain clarity for your day!";
+
+  console.log("Setting notification")
+
+  return Notifications.scheduleNotificationAsync({
+    content: {
+      title: title,
+      body: body,
+      data: {
+        name: "arun",
+      },
+    },
+    trigger: {
+      hour: 8,
+      minute: 0,
+      repeats: true
+    },
+  });
+}
 
 export const LifeVisionForm = () => {
   const [isEditting, setIsEditting] = useState(false)
 
   const lifeVision = useVisionStore(state => state.lifeVision)
   const lifeVisionDraft = useVisionStore(state => state.lifeVisionDraft)
+  const notificationId = useVisionStore(state => state.morningNotificationId)
 
   const updateLifeVision = useVisionStore(state => state.updateLifeVision)
   const updateLifeVisionDraft = useVisionStore(state => state.updateLifeVisionDraft)
+  const updateMorningNotificationId = useVisionStore(state => state.updateMorningNotificationId)
 
   const showForm = isEditting || !lifeVision
 
@@ -28,10 +53,15 @@ export const LifeVisionForm = () => {
     {showForm && (
       <>
         <TextInput value={lifeVisionDraft} onChangeText={updateLifeVisionDraft} />
-        <Button title="Save" onPress={() => {
+        <Button title="Save" onPress={async () => {
           updateLifeVision(lifeVisionDraft)
           updateLifeVisionDraft(undefined)
           setIsEditting(false)
+
+          if (!notificationId) {
+            const notificationId = await sendLocalNotification()
+            updateMorningNotificationId(notificationId)
+          }
         }} />
       </>
     )}
--- src/store/visionStorage.ts
+++ src/store/visionStorage.ts
@@ -6,20 +6,27 @@ import { persist, createJSONStorage } from "zustand/middleware"
 type State = {
   lifeVision: string | undefined
   lifeVisionDraft: string | undefined
+  morningNotificationId: string | undefined
 }
 
 type Actions = {
   updateLifeVision: (vision: string | undefined) => void
   updateLifeVisionDraft: (draft: string | undefined) => void
+  updateMorningNotificationId: (id: string | undefined) => void
 }
 
 export const useVisionStore = create<State & Actions>()(persist(
   immer((set) => ({
     lifeVision: undefined,
     lifeVisionDraft: undefined,
+    morningNotificationId: undefined,
     updateLifeVision: (vision: string | undefined) =>
       set((state) => {
         state.lifeVision = vision
+      }),
+      updateMorningNotificationId: (id: string | undefined) =>
+      set((state) => {
+        state.morningNotificationId = id
       }),
     updateLifeVisionDraft: (draft: string | undefined) =>
       set((state) => {
