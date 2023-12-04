e9f55d2ba6bba04398fd98eed5851ca258c4cc0b
ddeath <dusan.plavak@gmail.com>
2023-12-04 12:46:54 +0800

    Disable showing header

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,7 @@ const Stack = createNativeStackNavigator()
 
 export const Routes = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Dashboard" component={DashboardScreen} />
     </Stack.Navigator>
   )
